b71256cffa0256fb03013058a04fb1a3b914a301
Derry Everson <dneverson@lcmail.lcsc.edu>
2019-02-15 11:10:46 -0800

    Update main.js

--- HCCSearch/js/main.js
+++ HCCSearch/js/main.js
@@ -80,24 +80,6 @@ app.controller("MyCtrl", function($scope, $http){
     $scope.problems = [];
     searchStr = $scope.addSlashes(searchStr).replace(' ','[^"]+');
     var jsonStr = JSON.stringify($scope.jsonFile, null,' ');
-    // Matches and adds weight to current patient problems table
-    //var totalWeight = 0; //NOTE remove if not needed anymore
-    for(i=0;i<$scope.currentProblems.length;i++){
-      var icdcode = $scope.currentProblems[i].ICD10;
-      var found0 = jsonStr.match(
-        new RegExp(
-        '{\\s+"icd10":\\s".*'+ icdcode +'.*",'+
-        '\\s+"description":\\s".*",'+
-        '\\s+"weight":\\s[\\d\\.]+\\s+}'
-        ,'gi')
-      );
-      if(found0){
-        var normalized = (JSON.parse(JSON.stringify(found0).replace(/"{/g,"{").replace(/\\n/g,"").replace(/\\/g,"").replace(/}"/g,"}")));
-        $scope.currentProblems[i].Weight = normalized[0].Weight;
-        //totalWeight += normalized[0].Weight; //NOTE remove if not needed anymore
-      };
-    };
-    //$scope.currentProblems.unshift({"ICD10": "", "Description": "TOTAL WEIGHT:", "Weight": totalWeight}); //NOTE remove if not needed anymore
     // Search by Description
     var found1 = jsonStr.match(
       new RegExp(
@@ -121,19 +103,55 @@ app.controller("MyCtrl", function($scope, $http){
       $scope.problems = (JSON.parse(JSON.stringify(found2).replace(/"{/g,"{").replace(/\\n/g,"").replace(/\\/g,"").replace(/}"/g,"}")));
     }
   };
-  // gets and updates currentproblems for table
-  $scope.updateCurrentProblemsTable = function(){
-    $scope.currentProblems = [];
-    var currentProblemList = $scope.getCurrentProblems();
-    for(i=0;i<currentProblemList.length;i++){
-      if($scope.icd10Compare(currentProblemList[i])){
-        $scope.currentProblems.push({
-          ICD10: currentProblemList[i].ICD10.replace(".",""),
-          Description: currentProblemList[i].Description,
-          Weight: "TBD"
-        });
+  // Finds duplications in a json array
+  $scope.findDuplicates = function(arr){
+      var object = {};
+      var result = [];
+      arr.forEach(function (item) {
+        if(!object[item.ICD10])
+            object[item.ICD10] = 0;
+          object[item.ICD10] += 1;
+      })
+      for (var prop in object) {
+         if(object[prop] >= 2) {
+             result.push(prop);
+         }
+      }
+      return result;
+  };
+  // Adds text to the chart notes
+  $scope.addToChartNote = function(text){
+    var form_Id = window.external.FormId;
+    var document_variable = "DOCUMENT.TEMPHTML0_" + form_Id;
+    var text_translation = text;
+    window.external.SetChartValue(document_variable, text_translation);
+  };
+  // Checks and adds weights to current patient problems table
+  $scope.checkCurrentProblemWeights = function(){
+    $http.get('./data/Problems.json').then(function(response) {
+      // Can remove bottom 2 lines if no longer needed
+      var tmp = $scope.findDuplicates(response.data);
+      $scope.addToChartNote("Duplications: " + tmp);
+      var jsonStr1 = JSON.stringify(response.data, null,' ');
+      // Matches and adds weight to current patient problems table
+      var totalWeight = 0; //NOTE remove if not needed anymore
+      for(i=0;i<$scope.currentProblems.length;i++){
+        var icdcode = $scope.currentProblems[i].ICD10;
+        var found0 = jsonStr1.match(
+          new RegExp(
+          '{\\s+"icd10":\\s".*'+ icdcode +'.*",'+
+          '\\s+"description":\\s".*",'+
+          '\\s+"weight":\\s[\\d\\.]+\\s+}'
+          ,'gi')
+        );
+        if(found0){
+          var normalized = (JSON.parse(JSON.stringify(found0).replace(/"{/g,"{").replace(/\\n/g,"").replace(/\\/g,"").replace(/}"/g,"}")));
+          $scope.currentProblems[i].Weight = normalized[0].Weight;
+          //totalWeight += normalized[0].Weight; //NOTE remove if not needed anymore
+        };
       };
-    };
+      //$scope.currentProblems.unshift({"ICD10": "", "Description": "TOTAL WEIGHT:", "Weight": totalWeight}); //NOTE remove if not needed anymore
+    });
   };
   // Adds problem in current patient's problem list
   $scope.addProblem = function(type, desc, code, startDate, endDate, annotate, asmt, comm){
@@ -198,50 +216,6 @@ app.controller("MyCtrl", function($scope, $http){
     else result = false;
     return result;
   };
-  // Gets and parses current patient problems and pushes relevent information needed
-  $scope.getCurrentProblems = function(){
-    var prob = JSON.parse(window.external.problems);
-    var result = [];
-    for(i=0; i<prob.length; i++){
-      result.push({
-        pid: prob[i].problemID,
-        ICD10: prob[i].icd10CodeDetail.code,
-        Description: prob[i].icd10CodeDetail.longDescription,
-        stopdate: prob[i].stopdate,
-        stopreason: prob[i].stopreason
-      });
-    }
-    return result;
-  };
-  // Runs the addProblem function and safeguards from adding multipule problems
-  $scope.addButton = function(problem){
-    var currentdate = $scope.getCurrentDate();
-    var icd10Code = $scope.icd10Parse(problem.ICD10);
-    var currprob = $scope.getCurrentProblems();
-    var active = false;
-    for(i=0;i<currprob.length;i++){
-      if($scope.icd10Compare(icd10Code, currprob[i])){
-        active = true;
-      }
-      if(!active && i == currprob.length-1){
-        $scope.addProblem("DX OF",problem.Description,"ICD10-"+icd10Code, currentdate, "", "" , "N", "");
-      }
-    }
-    $scope.updateCurrentProblemsTable();
-  };
-  // Runs the removeProblem function and safeguards from removing multipule problems
-  $scope.removeButton = function(problem){
-    var currentdate = $scope.getCurrentDate();
-    var icd10Code = $scope.icd10Parse(problem.ICD10);
-    var currprob = $scope.getCurrentProblems();
-    for(i=0;i<currprob.length;i++){
-      if($scope.icd10Compare(icd10Code, currprob[i])){
-        $scope.removeProblem(currprob[i].pid, currentdate, false, "Resolved");
-        break;
-      }
-    }
-    $scope.updateCurrentProblemsTable();
-  };
   // checks to see if the add or remove button needs to be in place
   $scope.checkList = function(problem){
     try{
@@ -293,6 +267,81 @@ app.controller("MyCtrl", function($scope, $http){
       document.getElementById(element).children[0].classList.add(cssClass);
     }
   };
+  $scope.toggleProblemsTable = function(element, cssClass){
+    if (document.getElementById(element).classList.contains(cssClass)){
+      document.getElementById(element).classList.remove(cssClass);
+    }else{
+      document.getElementById(element).classList.add(cssClass);
+    }
+  };
+  // Gets and parses current patient problems and pushes relevent information needed
+  $scope.getCurrentProblems = function(){
+    var prob = JSON.parse(window.external.problems);
+    var result = [];
+    for(i=0; i<prob.length; i++){
+      try{
+        var isICD10Valid = (prob[i].icd10CodeDetail != null)?true:false;
+        result.push({
+          pid: prob[i].problemID,
+          ICD10: (isICD10Valid)?prob[i].icd10CodeDetail.code:"NA",
+          Description: (isICD10Valid)?prob[i].icd10CodeDetail.longDescription:"NA",
+          stopdate: prob[i].stopdate,
+          stopreason: prob[i].stopreason
+        });
+      }catch(err){
+        window.alert(err);
+      }
+    }
+    return result;
+  };
+  // gets and updates currentproblems for table
+  $scope.updateCurrentProblemsTable = function(){
+    $scope.currentProblems = [];
+    var currentProblemList = $scope.getCurrentProblems();
+    for(i=0;i<currentProblemList.length;i++){
+      if($scope.icd10Compare(currentProblemList[i])){
+        $scope.currentProblems.push({
+          ICD10: currentProblemList[i].ICD10.replace(".",""),
+          Description: currentProblemList[i].Description,
+          Weight: 0
+        });
+      };
+    };
+    $scope.checkCurrentProblemWeights();
+  };
+  // Runs the addProblem function and safeguards from adding multipule problems
+  $scope.addButton = function(problem){
+    var currentdate = $scope.getCurrentDate();
+    var icd10Code = $scope.icd10Parse(problem.ICD10);
+    var currprob = $scope.getCurrentProblems();
+    var active = false;
+    if(currprob.length == 0){
+      $scope.addProblem("DX OF",problem.Description,"ICD10-"+icd10Code, currentdate, "", "" , "N", "");
+    }else{
+      for(i=0;i<currprob.length;i++){
+        if($scope.icd10Compare(icd10Code, currprob[i])){
+          active = true;
+        }
+        if(!active && i == currprob.length-1){
+          $scope.addProblem("DX OF",problem.Description,"ICD10-"+icd10Code, currentdate, "", "" , "N", "");
+        }
+      }
+    }
+    $scope.updateCurrentProblemsTable();
+  };
+  // Runs the removeProblem function and safeguards from removing multipule problems
+  $scope.removeButton = function(problem){
+    var currentdate = $scope.getCurrentDate();
+    var icd10Code = $scope.icd10Parse(problem.ICD10);
+    var currprob = $scope.getCurrentProblems();
+    for(i=0;i<currprob.length;i++){
+      if($scope.icd10Compare(icd10Code, currprob[i])){
+        $scope.removeProblem(currprob[i].pid, currentdate, false, "Resolved");
+        break;
+      }
+    }
+    $scope.updateCurrentProblemsTable();
+  };
 
   // ################## CALL FUNCTIONS ################
   $scope.addToElement("error", "Type in the search bar for results.");
@@ -302,6 +351,8 @@ app.controller("MyCtrl", function($scope, $http){
     $scope.updateCurrentProblemsTable();
   }catch(err){
     $scope.hideElement("problemsTable");
+    $scope.hideElement("hoverBtn");
+    $scope.hideElement("updateCurrentProblemsBtn");
     $scope.currentProblems = [];
   }
 });
